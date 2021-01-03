5d735f43bcd4d3c2514c3665e69f3e3e6be9874e
Luz Vazquez Galindo <luzvazgal@MacBook-Air-de-Luz.local>
2021-01-03 16:33:35 -0600

    Adding filter by date

--- static/js/app.js
+++ static/js/app.js
@@ -5,19 +5,76 @@ var tableData = data;
 //Initializing filters to null
 var dateFilter = null
 
-/*Displays data from data.js. If dateFilter (dateF) param is null, it displays all data; othwerwise, it will display only content according to
-user's input (date)*/
-function displayData(){
-    var tbody = d3.select("tbody");
+var tbody = d3.select("tbody");
+
+/**
+ * Filter data using date user's input. 
+ * Removes all previous displayed data in webpage
+ * Sets dateFilter variable to user's input to display only data according to user selection
+ */
+function filterByDate()
+{
+    //Setting date Filter to user's date input
+    dateFilter = document.getElementById("datetime").value
+
+    //Clear previous displayed results in page
+    var rows = tbody.selectAll("tr")
+    rows.remove()
+
+    //console.log("filter by date")
+    //console.log(dateFilter)
+
+    //Loading data according to selected Date
+    loadData()
+    
+}
+
+/**
+ * Displays data from data.js. If dateFilter (dateF) param is null, it displays all data; othwerwise, it will display only content according to
+user's input (date)
+ */
+function loadData(){
+
+    //Variable to define if user's input (date) matches any of dates in data.js
+    let isDateMatch = false;
 
     for(var i=0; i<tableData.length; i++){
-        //console.log(tableData[i])
-        let row = tbody.append("tr");
 
         //Adding dictionary record to 'record' variable
         let record = tableData[i]
+        
+        //If there's no date filter
+        if(dateFilter==null){
+            displayData(record)
+            //Not displaying alert if it's the initial load of data in webpage
+            isDateMatch = true;
+        }
+        //If date is equal, display data
+        else if (dateFilter==record["datetime"]){
+            console.log("else if "+record["datetime"])
+            displayData(record)
+            isDateMatch = true;
+            
+        }
+        
+    }
+
+    //If Date does not match with any of included in data.js
+    if(!isDateMatch){
+        alert("No records found for that date. Please type another date")
+    }
+}
+
+
+/**
+ * Function to display UFO's observations according to a dateFilter or all data
+ * @param {*} record  Record containing all information related to a user's UFO observations
+ */
+function displayData(record){
+
+    let row = tbody.append("tr");
     
-        //Retrieving dictionary values (a record) using key names
+    //Retrieving dictionary values (a record) using key names
         let datetime = record["datetime"]
         let city = record["city"]
         let state = record["state"]
@@ -28,35 +85,16 @@ function displayData(){
         //Although index.html is set as UTF-8 (unicode), it doesn't recognizes those charecters as they're coming from source (data.js)
         let comment = record["comments"].replace(/&#33/g, "!").replace(/&#39/g, "'").replace(/&#44/g, ",")
 
-        if(dateFilter==null){
+        //Appending cells with values 
+        row.append("td").text(datetime)
+        row.append("td").text(city)
+        row.append("td").text(state)
+        row.append("td").text(country)
+        row.append("td").text(shape)
+        row.append("td").text(record["durationMinutes"])
+        row.append("td").text(comment)
 
-            row.append("td").text(datetime)
-            row.append("td").text(city)
-            row.append("td").text(state)
-            row.append("td").text(country)
-            row.append("td").text(shape)
-            row.append("td").text(record["durationMinutes"])
-            row.append("td").text(comment)
-
-        }
-        else if (datetime==dateFilter){
-            row.append("td").text(datetime)
-            row.append("td").text(city)
-            row.append("td").text(state)
-            row.append("td").text(country)
-            row.append("td").text(shape)
-            row.append("td").text(record["durationMinutes"])
-            row.append("td").text(comment)
-        }
+       
     }
-}
-
-function filterByDate()
-{
-    dateFilter = document.getElementById("datetime")
-    displayData()
-    
-}
-
 
 
