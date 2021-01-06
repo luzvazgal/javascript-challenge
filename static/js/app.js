2af71bb05cadcec61b2a95be1fdce6f2128f8a46
luzvazgal <70495368+luzvazgal@users.noreply.github.com>
2021-01-06 02:00:03 -0600

    Changing iteration on data using forEach method
    Changing iteration on data using forEach method which reduces considerably the amount of code (and thus, its maintenance). It makes easier the reuse of code.

--- static/js/app.js
+++ static/js/app.js
@@ -2,10 +2,49 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-//Initializing filters to null
+//Initializing date filter to null
 var dateFilter = null
 
 var tbody = d3.select("tbody");
+
+
+/**
+ * Displays data from data.js. If dateFilter variable is null, it displays all data; othwerwise, 
+ * it will display only content according to user's input (date)
+ */
+function loadData(){
+    //Variable to define if user's input (date) matches any of dates in data.js
+    tableData.forEach( displayData )
+  
+}
+
+/**
+ * Function to display UFO's observations according to a dateFilter or all data
+ * @param {*} record  Record containing all information related to a user's UFO observations
+ */
+function displayData(record){
+
+    let row = tbody.append("tr");
+    
+    //Retrieving dictionary values (a record) using key names
+    let datetime = record.datetime
+    let city = record.city
+    let state = record.state
+    let country = record.country
+    let shape = record.shape
+   //Cleans comments by replacing &#44, &#39, &#33 characters by ','  ''', and '!' respectively
+   //Although index.html is set as UTF-8 (unicode), it doesn't recognizes those charecters as they're coming from source (data.js)
+   let comment = record.comments.replace(/&#33/g, "!").replace(/&#39/g, "'").replace(/&#44/g, ",");
+
+   //Appending cells with values 
+   row.append("td").text(record.datetime);
+   row.append("td").text(record.city);
+   row.append("td").text(record.state);
+   row.append("td").text(record.country);
+   row.append("td").text(record.shape);
+   row.append("td").text(record.durationMinutes);
+   row.append("td").text(comment);
+}
 
 /**
  * Filter data using date user's input. 
@@ -23,14 +62,22 @@ function filterByDate()
         var rows = tbody.selectAll("tr")
         rows.remove()
 
-        //Loading data according to selected Date
-        loadData()
+        //Reinitializing tableData to filtered data according to user's filter
+        tableData = tableData.filter(record=> record.datetime == dateFilter)
+
+        //Validates whether user's datetime input has results or not
+        if (tableData.length==0)
+            alert("No records found for that date. Please type another date")
+        //Otherwise, load results into HTML page
+        else
+            loadData()
+
+        //Reassigning original data value to tableData to be filtered again
+        tableData = data;
     }
     else{
         alert("Insert a valid date format mm/dd/yyyy")
     }
-    
-    
 }
 
 /**
@@ -40,12 +87,11 @@ function filterByDate()
 function validateDate()
 {
     let isValid = true;
-
-    console.log("Validación 0 " + dateFilter.match(/\//g))
+   // console.log("Validación 0 " + dateFilter.match(/\//g))
 
     //If date doesn't have either first or second '/', date format is not valid
     if (dateFilter.match(/\//g).length != 2 ){
-        console.log("Validación 1 " + dateFilter.match(/\//g).length)
+     //   console.log("Validación 1 " + dateFilter.match(/\//g).length)
         return false
     }
 
@@ -54,24 +100,23 @@ function validateDate()
     let year = dateFilter.substring(dateFilter.lastIndexOf('/')+1)
 
     //Validates if month is a number between 1 and 12
-    console.log("Validación 2a " + dateFilter.substring(0,1))
+    //console.log("Validación 2a " + dateFilter.substring(0,1))
     if(parseInt(month, 10)>=1 && parseInt(month, 10)<=12){
         month = parseInt(month, 10)
-        console.log("Validación 2")
+      //  console.log("Validación 2")
     }
     else
         return false
 
     //Validates if day is valid to month's input
-    console.log("Validación 3a " + parseInt(day, 10) + " month: "+month)
-
+    //console.log("Validación 3a " + parseInt(day, 10) + " month: "+month)
 
     switch (month){
         //Feb
         case 2:
             if(parseInt(day, 10)>=1 && parseInt(day, 10)<=28){
                 day = parseInt(day, 10)
-                console.log("Validación 4")
+//                console.log("Validación 4")
             }
             else
                 return false
@@ -82,10 +127,10 @@ function validateDate()
         case 6:
         case 9:
         case 11:
-            console.log("Entro switch")
+  //          console.log("Entro switch")
             if(parseInt(day, 10)>=1 && parseInt(day, 10)<=30){
                 day = parseInt(day, 10)
-                console.log("Validación 5")
+    //            console.log("Validación 5")
             }
             else
                 return false
@@ -93,10 +138,10 @@ function validateDate()
 
         //Jan, Mar, May, Jul, Aug, Oct, Dec
         default:
-            console.log("Entro switch default")
+      //      console.log("Entro switch default")
             if(parseInt(day, 10)>=1 && parseInt(day, 10)<=31){
                 day = parseInt(day, 10)
-                console.log("Validación 3")
+        //        console.log("Validación 3")
             }
             else
                 return false
@@ -104,10 +149,10 @@ function validateDate()
     }
 
     //Validates year is a valid four digit number
-    console.log("Validación 4a " + parseInt(year, 10))
+    //console.log("Validación 4a " + parseInt(year, 10))
     if (year.length !=4 || isNaN(parseInt(year, 10)))
     {
-        console.log("Validación 6")
+      //  console.log("Validación 6")
        return false
     }
    
@@ -116,72 +161,15 @@ function validateDate()
     return isValid;
 }
 
-/**
- * Displays data from data.js. If dateFilter (dateF) param is null, it displays all data; othwerwise, it will display only content according to
-user's input (date)
- */
-function loadData(){
-
-    //Variable to define if user's input (date) matches any of dates in data.js
-    let isDateMatch = false;
-
-    for(var i=0; i<tableData.length; i++){
-
-        //Adding dictionary record to 'record' variable
-        let record = tableData[i]
-        
-        //If there's no date filter
-        if(dateFilter==null){
-            displayData(record)
-            //Not displaying alert if it's the initial load of data in webpage
-            isDateMatch = true;
-        }
-        //If date is equal, display data
-        else if (dateFilter==record["datetime"]){
-            console.log("else if "+record["datetime"])
-            displayData(record)
-            isDateMatch = true;
-            
-        }
-        
-    }
-
-    //If Date does not match with any of included in data.js
-    if(!isDateMatch){
-        alert("No records found for that date. Please type another date")
-    }
-}
 
 
-/**
- * Function to display UFO's observations according to a dateFilter or all data
- * @param {*} record  Record containing all information related to a user's UFO observations
- */
-function displayData(record){
-
-    let row = tbody.append("tr");
+function fillSelectData()
+{
+    console.log("Get data")
     
-    //Retrieving dictionary values (a record) using key names
-        let datetime = record["datetime"]
-        let city = record["city"]
-        let state = record["state"]
-        let country = record["country"]
-        let shape = record["shape"]
-
-        //Cleans comments by replacing &#44, &#39, &#33 characters by ','  ''', and '!' respectively
-        //Although index.html is set as UTF-8 (unicode), it doesn't recognizes those charecters as they're coming from source (data.js)
-        let comment = record["comments"].replace(/&#33/g, "!").replace(/&#39/g, "'").replace(/&#44/g, ",")
-
-        //Appending cells with values 
-        row.append("td").text(datetime)
-        row.append("td").text(city)
-        row.append("td").text(state)
-        row.append("td").text(country)
-        row.append("td").text(shape)
-        row.append("td").text(record["durationMinutes"])
-        row.append("td").text(comment)
-
-       
-    }
-
+    datetime_list = tableData.map(function getDate(dt){
+        return "<option>" + dt.datetime + "</option>"
+    })
+    document.getElementById("date_time").innerHTML = [...new Set(datetime_list)];
+}
 
